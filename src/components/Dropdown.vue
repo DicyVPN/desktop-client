b6e67fa5dcb818d6256d63bf9c18a305e593bbfb
Mr7fenix <mr7fenix@gmail.com>
2023-03-02 11:49:48 +0100

    Add transition when toggling a country

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,14 +1,27 @@
 <template>
   <DropdownCountry country="ar" @click="open('ar')"/>
-  <div v-if="selectedCountry === 'ar'" class="flex flex-col gap-y-[2px]">
-    <Server city="Buonasires" country="ar"/>
-    <Server city="San Rafael" country="ar"/>
-    <Server city="Rafael Castillo" country="ar"/>
-    <Server city="Rafael Castilfhidhfuidsfibsdifbsdifbisdfibsdi sdbfsb dfilo" country="ar"/>
-  </div>
+  <Transition name="expand">
+    <div v-if="selectedCountry === 'ar'" class="flex flex-col gap-y-[2px]">
+      <Server city="Buonasires" country="ar"/>
+      <Server city="San Rafael" country="ar"/>
+      <Server city="Rafael Castillo" country="ar"/>
+      <Server city="Rafael Castilfhidhfuidsfibsdifbsdifbisdfibsdi sdbfsb dfilo" country="ar"/>
+    </div>
+  </Transition>
+
   <DropdownCountry country="it"/>
   <DropdownCountry country="gb"/>
   <DropdownCountry country="us"/>
+  <DropdownCountry country="ru"/>
+  <DropdownCountry country="tz"/>
+  <DropdownCountry country="tk"/>
+  <DropdownCountry country="pk"/>
+  <DropdownCountry country="uz"/>
+  <DropdownCountry country="ag"/>
+  <DropdownCountry country="mx"/>
+  <DropdownCountry country="il"/>
+  <DropdownCountry country="lu"/>
+  <DropdownCountry country="rs"/>
 </template>
 <script>
 import Server from "@/components/Server.vue";
@@ -26,8 +39,23 @@ export default {
     open(country) {
       if (country === this.selectedCountry) {
         this.selectedCountry = null
-      }else this.selectedCountry = country
+      } else this.selectedCountry = country
     }
   }
 }
 </script>
+<style>
+
+.expand-enter-active,
+.expand-leave-active {
+  transition: all 0.2s;
+  height: 100%;
+}
+.expand-enter-from,
+.expand-leave-to
+{
+  opacity: 0;
+  height: 0;
+}
+
+</style>
