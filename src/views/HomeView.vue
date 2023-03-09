4d49ca244039299dbd28926e0ac5da4f989915a0
Mr7fenix <mr7fenix@gmail.com>
2023-03-09 23:06:12 +0100

    Fixed map overflow and added map zoom controls

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,31 @@
-<script setup lang="ts">
-import Logo from "@/components/icons/Logo.vue";
-import Sidebar from "@/components/home/sidebar/Sidebar.vue";
-import MapCard from "@/components/home/map/MapCard.vue";
-import InformationCard from "@/components/home/InformationCard.vue";
-
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="flex flex-1 min-h-screen">
+  <div class="flex flex-1 min-h-screen max-h-screen min-w-screen max-w-screen">
     <Sidebar/>
-    <div class="flex flex-col">
+    <div class="flex flex-col flex-1">
       <div class="p-8">
         <div class="bg-gray-600 rounded p-8">
           <Logo/>
         </div>
       </div>
-      <MapCard class="flex-1"/>
+      <div class="flex-1 overflow-hidden">
+        <WorldMap class="bg-gray-800 overflow-hidden" data-zoom-on-wheel="zoom-amount: 0.006; min-scale: 1; max-scale: 4;"
+                  data-pan-on-drag/>
+      </div>
       <InformationCard/>
     </div>
   </div>
 </template>
+
+<script>
+import 'svg-pan-zoom-container';
+import Logo from "@/components/icons/Logo.vue";
+import Sidebar from "@/components/home/sidebar/Sidebar.vue";
+import InformationCard from "@/components/home/InformationCard.vue";
+import WorldMap from "@/components/home/map/WorldMap.vue";
+
+export default {
+  name: "HomeView",
+  components: {WorldMap, Logo, InformationCard, Sidebar}
+};
+</script>
 
