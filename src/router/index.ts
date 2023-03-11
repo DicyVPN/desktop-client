b0332f451a237952877130d898fc1aa741d35d2d
Mr7fenix <mr7fenix@gmail.com>
2023-03-11 01:05:27 +0100

    Added Placeholder view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
+import PlaceholderView from "@/views/PlaceholderView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      //path: '/',
-      path: '/ll',
+        path: '/',
+      name: 'placeholder',
+        component: PlaceholderView
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
     {
-      //path: '/login',
-      //name: 'login',
-      path: '/',
-      name: 'home',
+      path: '/login',
+      name: 'login',
       component: LoginView
     }
   ]
---
+++ src/views/PlaceholderView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-center items-center min-h-screen" @click="change">
+    <font-awesome-icon icon="fa-solid fa-circle-notch" class="icon-animation h-64 w-64"/>
+    <router-link to="/home">Home</router-link>
+    <router-link to="/login">Loading</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlaceholderView",
+  methods: {
+    change() {
+      //change view
+      this.$router.push({name: "LoginView"});
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+.icon-animation {
+  animation: ruota 2s infinite linear;
+}
+
+@keyframes ruota {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
