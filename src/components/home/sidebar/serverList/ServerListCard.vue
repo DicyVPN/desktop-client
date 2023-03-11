1523736cdda02abf12da65f6b5d0cf854039a1b5
Mr7fenix <mr7fenix@gmail.com>
2023-03-11 01:04:18 +0100

    Added Wiresock and connection to Server

--- src/components/home/sidebar/serverList/ServerListCard.vue
+++ src/components/home/sidebar/serverList/ServerListCard.vue
@@ -3,7 +3,7 @@
     <div class="sidebar-card-inner p-8 h-full flex flex-col gap-8 overflow-y-auto">
       <p class="ml-8 mt-8 text-small font-light">Server Consigliati</p>
       <div class="flex flex-col gap-y-[2px]" v-for="country in serverList.suggest">
-        <Server v-for="(city, name) in country.server" :country="country.tag" :city="name" :id="city.id"/>
+        <Server v-for="(city, name) in country.server" :country="country.tag" :city="name" :id="city.id" @click="connect"/>
       </div>
       <div>
         <p class="ml-8 text-small font-light">Altri Server</p>
@@ -19,12 +19,17 @@
 <script>
 import Dropdown from "@/components/home/sidebar/serverList/Dropdown.vue"
 import Server from "@/components/home/sidebar/serverList/Server.vue"
-
 export default {
   name: 'ServerListCard',
   components: {Dropdown, Server},
   props: {
     serverList: {}
+  },
+  methods: {
+    connect() {
+      window.api.startVPN()
+
+    }
   }
 }
 </script>
