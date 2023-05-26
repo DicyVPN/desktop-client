cd8e61de6d3c21b907fe0bffdf02b24d4c347f63
Lorenzo Lapucci <halexercraft@gmail.com>
2023-05-26 10:49:19 +0200

    Add scroll shadow on top of the list and conditionally show if has not reached top or bottom

--- src/components/home/sidebar/serverList/ServerCardList.vue
+++ src/components/home/sidebar/serverList/ServerCardList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sidebar-card h-full w-full relative overflow-hidden">
-        <div class="sidebar-card-inner p-8 h-full flex flex-col gap-8 overflow-y-auto">
+        <div class="sidebar-card-inner p-8 h-full flex flex-col gap-8 overflow-y-auto" @scroll="updateShadows">
             <p class="ml-8 mt-8 text-small font-light">Server Consigliati</p>
             <PrimaryServers :list="list"/>
             <div>
@@ -11,7 +11,8 @@
                 <Dropdown :list="list"/>
             </div>
         </div>
-        <div class="bottom-shadow absolute shadow-personal"></div>
+        <div class="scroll-shadow-top absolute" :class="{'top-shadow': showTopShadow}"></div>
+        <div class="scroll-shadow-bottom absolute" :class="{'bottom-shadow': showBottomShadow}"></div>
     </div>
 </template>
 <script>
@@ -20,12 +21,17 @@ import PrimaryServers from "@/components/home/sidebar/serverList/primary/Primary
 import {throwError} from "@/global";
 
 export default {
-    name: 'ServerListCard',
     components: {PrimaryServers, Dropdown},
     props: {
         list: {
             type: Object,
             required: true
+        }
+    },
+    data() {
+        return {
+            showTopShadow: false,
+            showBottomShadow: true
         }
     },
     methods: {
@@ -35,10 +41,15 @@ export default {
             } catch (e) {
                 throwError("Errore di connessione al server, riprova")
             }
+        },
+        updateShadows({ target: { scrollTop, clientHeight, scrollHeight }}) {
+            this.showTopShadow = scrollTop > 12;
+            this.showBottomShadow = scrollTop + clientHeight < scrollHeight - 12;
         }
     }
 }
 </script>
+
 <style scoped>
 .sidebar-card-inner::-webkit-scrollbar {
     width: 8px;
@@ -58,12 +69,37 @@ export default {
     background: black;
 }
 
-.bottom-shadow {
+
+@property --shadow-color {
+    syntax: '<color>';
+    initial-value: rgb(0 0 0 / 0);
+    inherits: false;
+}
+
+.scroll-shadow-top, .scroll-shadow-bottom {
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
-    background-image: linear-gradient(to top, hsl(222, 10%, 5%), rgb(0 0 0 / 0) 3.6rem, rgb(0 0 0 / 0));
     pointer-events: none;
+    --shadow-color: rgb(0 0 0 / 0);
+    background-image: linear-gradient(var(--shadow-direction), var(--shadow-color), rgb(0 0 0 / 0) 3.6rem, rgb(0 0 0 / 0));
+    transition: --shadow-color 200ms ease-in-out;
+}
+
+.scroll-shadow-top {
+    --shadow-direction: to bottom;
+}
+
+.scroll-shadow-bottom {
+    --shadow-direction: to top;
+}
+
+.top-shadow {
+    --shadow-color: hsl(222, 10%, 5%);
+}
+
+.bottom-shadow {
+    --shadow-color: hsl(222, 10%, 5%);
 }
 </style>
