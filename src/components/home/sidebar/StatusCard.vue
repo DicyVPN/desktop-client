a6b672294da056d3f637474bdbeb567528e7403d
Lorenzo Lapucci <halexercraft@gmail.com>
2023-06-26 19:28:24 +0200

    Remove outline that appears when focusing the window

--- src/components/home/sidebar/StatusCard.vue
+++ src/components/home/sidebar/StatusCard.vue
@@ -6,7 +6,7 @@
                                    class="text-bright-green"/>
                 <font-awesome-icon v-else icon="fa-solid fa-circle-xmark" class="text-red-300"/>
                 <p class="text-small font-light">{{ currentServer.connected ? "Connesso" : "Disconnesso" }}</p>
-                <router-link to="/settings/general" class="flex items-center w-full hover:text-gray-200">
+                <router-link to="/settings/general" class="flex items-center w-full hover:text-gray-200 outline-none">
                     <font-awesome-icon icon="fa-solid fa-gear" class="ml-auto"/>
                 </router-link>
 
