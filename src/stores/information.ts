ed07f119b25cf46b970c8789215cc84c371acb1d
Lorenzo Lapucci <halexercraft@gmail.com>
2023-07-13 16:06:05 +0200

    Use "..." instead of "000.000.000.000" as default IP value

--- src/stores/information.ts
+++ src/stores/information.ts
@@ -4,7 +4,7 @@ import {defineStore} from 'pinia'
 export const useInformationStore = defineStore('information', {
     state: () => {
         return ({
-            ip: '000.000.00.000'
+            ip: '...'
         })
     },
 })
