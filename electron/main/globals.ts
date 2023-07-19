2e2c0a6d8110f32eda9b1d8a6e711f30ed9d33b2
Lorenzo Lapucci <halexercraft@gmail.com>
2023-07-19 10:56:17 +0200

    Get WireGuard installation path from the registry key

--- electron/main/globals.ts
+++ electron/main/globals.ts
@@ -1,3 +1,5 @@
+import {registry} from 'windows';
+
 const userDataPath = process.env.APPDATA ?? (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share');
 export const DATA_PATH = userDataPath + '/DicyVPN';
 export const EXECUTABLE_NAME = 'DicyVPN.exe';
@@ -12,8 +14,8 @@ export function getWireGuardClientPath() {
     if (process.platform !== 'win32') {
         throw new Error('WireGuard is only supported on Windows');
     }
-    const programFiles = process.env['ProgramFiles'] ?? 'C:\\Program Files';
-    return programFiles + '\\WireSock VPN Client\\bin\\wiresock-client.exe';
+    const keySet = registry('HKLM\\SOFTWARE\\NTKernelResources\\WinpkFilterForVPNClient');
+    return keySet.InstallLocation.valueOf() + 'bin\\wiresock-client.exe';
 }
 
 export function getOpenVPNServicePath() {
--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,41 @@
 /// <reference types="vite/client" />
+
+
+declare module 'windows' {
+    export function registry(key: string): KeySet;
+
+    interface KeySet {
+        // Remove a KeySet recursively or a child
+        remove(name: string): boolean | string;
+
+        // Add a new child to this KeySet
+        add(name: string, value: any): boolean;
+
+        // Get a child KeySet
+        [name: string]: KeySet | Entry;
+    }
+
+    interface Entry {
+        // Remove the value from the registry. It's still available in JavaScript though
+        remove(): boolean;
+
+        inspect(): any;
+
+        valueOf(): any;
+
+        parent: KeySet;
+        name: string;
+        value: any;
+        type: RegType;
+        raw: any;
+    }
+
+    type RegType =
+        'REG_NONE'
+        | 'REG_SZ'
+        | 'REG_EXPAND_SZ'
+        | 'REG_BINARY'
+        | 'REG_DWORD'
+        | 'REG_MULTI_SZ'
+        | 'REG_QWORD';
+}
